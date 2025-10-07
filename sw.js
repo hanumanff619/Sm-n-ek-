432eaabd6ccded61872b8b866662fdf6a8260be8
hanumanff619 <mckra@seznam.cz>
2025-10-08 01:10:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SW v170 – cache bump
-const CACHE='smenarek-cache-v170';
-const ASSETS=['./','./index.html','./app.js?v=170','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='smenarek-cache-v171';
+const ASSETS=['./','./index.html','./app.js?v=171','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{

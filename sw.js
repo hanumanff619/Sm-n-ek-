efd412e7fd9bec3d235ee6eaac8eabae56869827
hanumanff619 <mckra@seznam.cz>
2025-10-13 00:04:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,2 @@
-// SW v181 – cache bump
-const CACHE='smenarek-cache-v181';
-const ASSETS=['./','./index.html','./app_v181.js?v=181','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./backgrounds/bg_12h.jpg','./backgrounds/bg_8h.jpg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))) .then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  const critical = u.pathname.endsWith('/app_v181.js') || u.pathname.endsWith('/index.html');
-  if(critical){
-    e.respondWith(fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;}).catch(()=>caches.match(e.request)));
-  }else{
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
-});
+self.addEventListener('install', e=>{ self.skipWaiting(); });
+self.addEventListener('activate', e=>{ e.waitUntil(clients.claim()); });
